f33ee9d5484bb2d44626a876e3a5ca60962adb4c
Vagner <sinxberserker@gmail.com>
2020-10-16 17:15:03 -0300

    Pacote com as camadas MVC e a classe principal

---
+++ src/main/java/br/com/vagner/phenomenal/webservice/PhenomenalWebServiceApplication.java
@@ -0,0 +1,20 @@
+package br.com.vagner.phenomenal.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe princilap que executa o servidor
+ * @author Vagner
+ *
+ */
+@SpringBootApplication
+public class PhenomenalWebServiceApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(PhenomenalWebServiceApplication.class, args);
+	}
+
+}
